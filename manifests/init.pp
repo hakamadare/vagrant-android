e5901b9756448452389b1dd94de8c3562f34de76
Steve Huff <steve.huff@runkeeper.com>
2014-12-29 10:11:20 -0500

    not the init file's problem

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,21 @@
 node default {
   # Setup pre and post run stages
   # Typically these are only needed in special cases but are good to have
-  # stage { ['pre', 'post']: }
-  # Stage['pre'] -> Stage['main'] -> Stage['post']
-  #
-  # $platforms   = prefix('android-', $::android_platforms)
-  # $build_tools = prefix('build-tools-', $::android_build_tools)
-  #
-  # # Install JDK
-  # class { 'java': distribution => 'jdk' } ->
-  #
-  # # Install Android SDK
-  # class { 'android': install_dir => '/opt/android' } ->
-  #
-  # # Install platform and build tools
-  # android::platform { $platforms: } ->
-  # android::build_tools { $build_tools: }
+  stage { ['pre', 'post']: }
+  Stage['pre'] -> Stage['main'] -> Stage['post']
 
-  notify { 'Do a thing': }
+  $platforms   = prefix('android-', $::android_platforms)
+  $build_tools = prefix('build-tools-', $::android_build_tools)
+
+  # Install JDK
+  class { 'java': distribution => 'jdk' } ->
+
+  # Install Android SDK
+  class { 'android': install_dir => '/opt/android' } ->
+
+  # Install platform and build tools
+  android::platform { $platforms: } ->
+  android::build_tools { $build_tools: }
+
 }
 # vim: set ft=puppet ts=2 sw=2 ei:
